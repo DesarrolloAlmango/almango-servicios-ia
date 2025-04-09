dc960b4fc84895a51887fc45e3bec37a434ad1a2
Rodrigo Salas <salasrodrigo111@gmail.com>
2025-04-09 16:56:52 +0200

    cambio

--- src/pages/Servicios.tsx
+++ src/pages/Servicios.tsx
@@ -49,7 +49,7 @@ const fetchTarjetasServicios = async (): Promise<TarjetaServicio[]> => {
   try {
     // Use the proxy to avoid CORS issues
     const response = await fetch(
-      "/api/AlmangoAPINETFrameworkSQLServer/APIAlmango/GetTarjetasServicios",{
+      "/AlmangoAPINETFrameworkSQLServer/APIAlmango/GetTarjetasServicios",{
         "headers": {
           "accept": "text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.7",
           "accept-language": "es-ES,es;q=0.9",
